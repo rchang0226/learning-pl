504991aa5dae907e40f9c301be486caba1aef28e
Raymond Chang <rchang0226@gmail.com>
2022-08-22 23:12:05 -0400

    Define discriminator

--- gan.py
+++ gan.py
@@ -18,14 +18,40 @@ class Discriminator(pl.LightningModule):
         self.learning_rate = learning_rate
 
         self.model = nn.Sequential(
-            nn.Conv2d(3, 64, 3, stride=2, padding='same'),
+            nn.Conv2d(channels, 64, 3, stride=2, padding='same'),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2),
-            nn.Conv2d(3, 64, 3, stride=2, padding='same'),
+            nn.Conv2d(channels, 64, 3, stride=2, padding='same'),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2),
             nn.Flatten(),
             nn.Linear(64 * width / 4 * height / 4, 1),
-            nn.Sigmoid()  # remove this later, using a loss function with sigmoid attached
         )
 
+        self.loss_fn = nn.BCEWithLogitsLoss()
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        preds = logits.squeeze(dim=1)
+        loss = self.loss_fn(preds, y)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        preds = logits.squeeze(dim=1)
+        loss = self.loss_fn(preds, y)
+        preds = torch.sigmoid(preds)
+        pred_labels = (preds >= 0.5).long()
+        acc = accuracy(pred_labels, y)
+        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_acc", acc, prog_bar=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate, betas=(0.5, 0.999))
+        return optimizer
